5d338f0239244c69d9be83402773173986d65e24
Edikan <victrudoh@gmail.com>
2022-10-24 12:48:32 +0100

    adding access control to headers in index.routes

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -4,16 +4,14 @@ const adminRouter = require("./admin.routes");
 const caregiverRouter = require("./caregiver.routes");
 
 module.exports = function (app) {
-  app.use(function (req, res, next) {
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Access-Control-Allow-Origin",
-      "x-access-token, Origin, Content-Type, Accept"
-    );
-    next();
-  });
+    app.use(function (req, res, next) {
+        res.header(
+            "Access-Control-Allow-Headers", "x-access-token, Origin, Content-Type, Accept"
+        );
+        next();
+    });
 
-  app.use("/api/auth", authRouter);
-  app.use("/api/admin", adminRouter);
-  app.use("/api/caregiver", caregiverRouter);
+    app.use("/api/auth", authRouter);
+    app.use("/api/admin", adminRouter);
+    app.use("/api/caregiver", caregiverRouter);
 };
