6859d21f6b925c52a4f5bbc1217687d4ff987e2e
Edikan <victrudoh@gmail.com>
2022-10-24 13:34:34 +0100

    CORS debugging

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -5,7 +5,10 @@ const caregiverRouter = require("./caregiver.routes");
 
 module.exports = function (app) {
   app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Headers", "*");
+    res.header(
+      "Access-Control-Allow-Origin",
+      "x-access-token, Origin, Content-Type, Accept"
+    );
     next();
   });
 
