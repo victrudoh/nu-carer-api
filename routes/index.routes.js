273760a6395181f637b1dd5cb7ee9cc03f0dc97b
Edikan <victrudoh@gmail.com>
2022-10-24 13:23:44 +0100

    Still trying to fix the CORS error in index.routes

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -4,14 +4,12 @@ const adminRouter = require("./admin.routes");
 const caregiverRouter = require("./caregiver.routes");
 
 module.exports = function (app) {
-    app.use(function (req, res, next) {
-        res.header(
-            "Access-Control-Allow-Headers", "x-access-token, Origin, Content-Type, Accept"
-        );
-        next();
-    });
+  app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Headers", "*");
+    next();
+  });
 
-    app.use("/api/auth", authRouter);
-    app.use("/api/admin", adminRouter);
-    app.use("/api/caregiver", caregiverRouter);
+  app.use("/api/auth", authRouter);
+  app.use("/api/admin", adminRouter);
+  app.use("/api/caregiver", caregiverRouter);
 };
